9bf188556629f1199c6e0faeb638b6f917d8efcd
Chris Manson <chris@stonecircle.io>
2018-02-24 21:25:50 +0000

    removing npm upgrade

--- lib/binaries.sh
+++ lib/binaries.sh
@@ -17,29 +17,3 @@ install_nodejs() {
   mv /tmp/node-v$number-$os-$cpu/* $dir
   chmod +x $dir/bin/*
 }
-
-install_npm() {
-  local version="$1"
-  local dir="$2"
-  local npm_lock="$3"
-  local npm_version="$(npm --version)"
-
-  # If the user has not specified a version of npm, but has an npm lockfile
-  # upgrade them to npm 5.x if a suitable version was not installed with Node
-  if $npm_lock && [ "$version" == "" ] && [ "${npm_version:0:1}" -lt "5" ]; then
-    echo "Detected package-lock.json: defaulting npm to version 5.x.x"
-    version="5.x.x"
-  fi
-
-  if [ "$version" == "" ]; then
-    echo "Using default npm version: `npm --version`"
-  elif [[ `npm --version` == "$version" ]]; then
-    echo "npm `npm --version` already installed with node"
-  else
-    echo "Bootstrapping npm $version (replacing `npm --version`)..."
-    if ! npm install --unsafe-perm --quiet -g "npm@$version" 2>@1>/dev/null; then
-      echo "Unable to install npm $version; does it exist?" && false
-    fi
-    echo "npm `npm --version` installed"
-  fi
-}
